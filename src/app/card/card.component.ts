bc8405233e85591ecf1c56d676489782da11035b
Christopher <cristopherb72@outlook.com>
2021-10-16 14:38:25 -0600

    New header for Search and new changes  json

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,11 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  public image!: string;
   constructor() { }
 
   ngOnInit(): void {
-    this.image = 'https://pbs.twimg.com/media/Dybd_Y9XQAAiItW?format=jpg&name=900x900'
     console.log('Llamando componente');
   }
 
